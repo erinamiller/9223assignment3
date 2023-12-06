e53d97432e0dd1a7614fd5fd66a07b3867a0793d
erinvmiller <erin.miller@wustl.edu>
2023-12-06 16:45:31 -0500

    Edits to api service structure, added form to upload

--- frontend/src/Upload.jsx
+++ frontend/src/Upload.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import APIService from "./APIService";
 
 export default function Upload() {
+    const [image, setImage] = useState(null);
+    const [labels, setLabels] = useState("");
+    
+    const handleImageChange = (e) => {
+        setImage(e.target.files[0]);
+    };
+    const handleLabelsChange = (e) => {
+        setLabels(e.target.value);
+    };
+    const handleSubmit = () => {
+        if (image) {
+            const formData = new FormData(); // not sure if i want to use this or do something custom
+            formData.append("image", image);
+            formData.append("labels", labels);
+            // APIService.uploadPhoto(formData)
+            APIService.uploadPhoto(image, labels.split(","))
+                .then((response) => {
+                    console.log(response);
+                })
+                .catch((error) => {
+                    console.error(error);
+                });
+        }
+    };
     return (
-        <h1>HI</h1>
+        <div>
+            <input type="file" onChange={handleImageChange} />
+            <input type="text" placeholder="Labels (comma separated)" value={labels} onChange={handleLabelsChange} />
+            <button onClick={handleSubmit}>Submit</button>
+        </div>
     );
 }
